e7167f811bc1bb96af6393883de84b1152544b6f
Harshit Jain <harshitostwal1234@gmail.com>
2024-08-27 14:48:18 +0530

    Created Slider For Images In Home Page

---
+++ app/(Home)/components/Slider.jsx
@@ -0,0 +1,28 @@
+"use client";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import React from 'react';
+import { Autoplay, EffectFade } from 'swiper/modules';
+import { SliderData } from '@/data/Home/Slider';
+import Image from 'next/image';
+import 'swiper/css/effect-fade';
+
+export function Slider() {
+
+    return (
+        <Swiper
+            effect={'fade'}
+            autoplay={{
+                delay: 7000,
+            }}
+            loop={true}
+            modules={[Autoplay, EffectFade]}
+        >
+            {SliderData.map((data, index) => (
+                <SwiperSlide key={index}>
+                    <Image src={data.imageUrl} alt="Dg Vaishnav College" className="w-full h-[13rem] sm:h-[20rem] md:h-[30rem] lg:h-[34rem] 2xl:h-[45rem]" fetchPriority="high" loading="eager" quality={100} />
+                </SwiperSlide>
+            ))}
+        </Swiper>
+    )
+}
---
+++ data/Home/Slider.js
@@ -0,0 +1,19 @@
+import I1 from "@/public/Home/Slider/1.gif"
+import I2 from "@/public/Home/Slider/2.jpg"
+import I3 from "@/public/Home/Slider/3.gif"
+import I4 from "@/public/Home/Slider/4.jpg"
+
+export const SliderData = [
+    {
+        imageUrl: I1
+    },
+    {
+        imageUrl: I2
+    },
+    {
+        imageUrl: I3
+    },
+    {
+        imageUrl: I4
+    }
+]
