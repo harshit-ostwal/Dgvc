34287a3a3600fba7d7d9fcd79f11b210d8fd5bdf
Harshit Jain <harshitostwal1234@gmail.com>
2024-08-27 14:47:15 +0530

    Created New App & Installed Some Packages, & Added Fonts

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import { Slider } from './(Home)/components/Slider';
+import React from 'react';
+
+function Page() {
+
+  return (
+    <div className="w-full min-h-screen">
+      <Slider />
+    </div>
+  );
+}
+
+export default Page;
