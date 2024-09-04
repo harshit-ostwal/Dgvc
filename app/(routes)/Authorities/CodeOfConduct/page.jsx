fd217c6ca756c787a96cb4c6830de1a58a1ae400
Harshit Jain <harshitostwal1234@gmail.com>
2024-09-05 03:55:02 +0530

    Created Code Of Conduct

---
+++ app/(routes)/Authorities/CodeOfConduct/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+    return (
+        <div className="flex flex-col items-center justify-center w-full min-h-screen gap-40 py-40">
+            <h1 className="text-4xl font-semibold lg:text-6xl">Code Of Conduct</h1>
+        </div>
+    )
+}
+
+export default page
